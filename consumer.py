b57c3253d5befa43c782b5a8dd77db45a89d785e
David Edson <dave@stibosystems.com.au>
2024-09-18 14:14:24 +1000

    local

--- consumer.py
+++ consumer.py
@@ -6,12 +6,15 @@ from kafka import KafkaConsumer
 from Perdy.pretty import prettyPrintLn
 from Perdy.parser import printXML
 
-consumer = KafkaConsumer(bootstrap_servers='voldemort:9092')
+consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
 consumer.subscribe(['quickstart-events'])
+
+horizon = '_'*int(os.environ['COLUMNS'])
 
 for message in consumer:
 	text = message.value.decode('utf8')
 	try:
+		print(horizon)
 		d = json.loads(text)
 		prettyPrintLn(d)
 	except:
--- publish-to-queue.sh
+++ publish-to-queue.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-bin/kafka-console-producer.sh --topic quickstart-events --bootstrap-server voldemort:9092
+bin/kafka-console-producer.sh --topic quickstart-events --bootstrap-server localhost:9092
 #This is my first event
 #This is my second event
